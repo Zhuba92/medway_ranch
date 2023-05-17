7aba430aa0ea5698a34a9cef78611bbe2f5612ca
Zhuba92 <zachary.huba92@gmail.com>
2023-05-17 17:01:13 -0500

    minor setup stuff

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,27 @@
 <template>
   <v-app-bar :elevation="2" rounded class="bg-orange-accent-1">
 
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon
+      @click="openNavMenu"
+    ></v-app-bar-nav-icon>
 
-    <v-app-bar-title>Medway Ranch</v-app-bar-title>
+    <v-app-bar-title>Medway Ranch {{ this.greeting }}</v-app-bar-title>
 
   </v-app-bar>
 </template>
 
-<script setup>
-  //
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello World!'
+    }
+  },
+
+  methods: {
+    openNavMenu() {
+      console.log('hello');
+    },
+  },
+}
 </script>
