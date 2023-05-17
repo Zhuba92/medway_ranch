1d7e438bd093c3076c90332097b1eb72ae67dc60
Zhuba92 <zachary.huba92@gmail.com>
2023-05-17 17:33:01 -0500

    minor setup stuff

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,26 +1,53 @@
 <template>
   <v-app-bar :elevation="2" rounded class="bg-orange-accent-1">
 
-    <v-app-bar-nav-icon
-      @click="openNavMenu"
-    ></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-    <v-app-bar-title>Medway Ranch {{ this.greeting }}</v-app-bar-title>
+    <v-toolbar-title>Medway Ranch</v-toolbar-title>
+
+    <v-spacer></v-spacer>
 
   </v-app-bar>
+
+  <v-navigation-drawer
+    v-model="drawer"
+    temporary
+  >
+    <v-list
+      :items="items"
+    ></v-list>
+  </v-navigation-drawer>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      greeting: 'Hello World!'
+      drawer: false,
+      group: null,
+      items: [
+        {
+          title: 'About',
+          value: 'about',
+        },
+        {
+          title: 'Contact',
+          value: 'contact',
+        },
+      ],
     }
   },
 
   methods: {
     openNavMenu() {
       console.log('hello');
+    },
+  },
+
+  watch: {
+    group () {
+      this.drawer = false
     },
   },
 }
